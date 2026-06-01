99be2860d868f743c84d3b3909715d64fd6ade4a
Saif <saifeldeen409@gmail.com>
2026-06-01 05:18:14 +0300

    fix

--- backend/utils/generateTokenAndSetCookie.js
+++ backend/utils/generateTokenAndSetCookie.js
@@ -5,7 +5,7 @@ export const generateTokenAndSetCookie = (res, userId) => {
     res.cookie("token", token, {
         httpOnly: true,
         secure: process.env.NODE_ENV === "production",
-        sameSite: "strict",
+        sameSite: "none",
         maxAge: 7 * 24 * 60 * 60 * 1000
     })
     return token;
