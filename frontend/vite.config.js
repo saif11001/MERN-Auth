7103c1e665005777fa1188c181e1be06da902ab7
Saif <saifeldeen409@gmail.com>
2026-06-01 05:24:04 +0300

    fix vercel config

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -2,15 +2,17 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(),
-  ],
+  plugins: [react(), tailwindcss()],
+  build: {
+    outDir: 'dist',
+  },
   server: {
     proxy: {
-      '/api': 'http://localhost:5000'
+      '/api': {
+        target: 'http://localhost:5000',
+        changeOrigin: true,
+      }
     }
   }
 })
